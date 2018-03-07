903fd25d5c65e790e85615768b66f5c5379374d4
Abubakr Mahmood <2257419m@student.gla.ac.uk>
2018-03-07 14:37:19 +0000

    Ch.4

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Game Guager! <br/> <a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': "this is the bold text from views.py"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("this is the about page <br/> <a href='/rango/'>Index</a>")
