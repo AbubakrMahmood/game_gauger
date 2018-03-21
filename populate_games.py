4b9adbd32670a5e1cb680f77d20708a03ac0dead
Abubakr Mahmood <2257419m@student.gla.ac.uk>
2018-03-21 16:24:38 +0000

    Population, not tested - Abz

---
+++ populate_games.py
@@ -0,0 +1,50 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'game_gauger.settings')
+
+import django
+django.setup()
+from reviews.models import Game, Reviews
+
+def populate();
+    # list of dictionaries containing reviews to add to each game
+    batman_arkham = [
+        {"user_name" : "Bob",
+         "comment" : "This is a comment",
+         "rating" : "3"} ]
+
+    games = {"Batman: Arkam Asylum":{"reviews":batman_arkham} }
+
+    # goes through the games dictionary, then adds each game,
+    # and then adds all the associated reviews for that game
+    for game, game_data in games.items():
+        g = add game()
+        for r in game_data["reviews"]:
+            add_review(g, r["user_name"], r["comment"], r["rating"])
+
+    # Print out the games we have added
+    for g in Game.objects.all():
+        for r in Reviews.objects.filter(game=g):
+            print("- {0} - {1}".format(str(g), str(r)))
+
+
+def add_review(game, user_name, comment, rating=0):
+    r = Reviews.objects.get_or_create(game=game)[0]
+    r.user_name = user_name
+    r.comment = comment
+    r.rating = rating
+    r.save()
+    return r
+
+def add_game(name, genre, publisher, developer, image):
+    g = Game.objects.get_or_create(name=name, genre=genre, publisher=publisher
+                                   developer=developer, image=image)[0]
+    g.save()
+    return g
+
+if __name__ == '__main__':
+    print("Starting Review population script...")
+    populate()
+
+
+
--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from reviews.models import Game
 
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage': "this is the bold text from views.py"}
+    game_list = Game.objects.all()
+    context_dict = {'games': game_list}
     return render(request, 'reviews/index.html', context=context_dict)
 
 def about(request):
