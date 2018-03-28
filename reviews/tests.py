445375460356ef09de563c6921b7d9e877116ec6
MegaAbz <2257419m@student.gla.ac.uk>
2018-03-28 05:07:09 +0000

    Minor changes - Abz

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from reviews.models import Game
 
-# Create your tests here.
+class GameMethodTests(TestCase):
+
+
+#methods needing tested:
+#rating choice
+#Im size
+#logo is null
+
+#View testing
+#If user form is invalid-- error
+#Uder login error message
+
